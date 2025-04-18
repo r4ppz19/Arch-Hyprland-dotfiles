a93479692e552106fab086dd301bb36b6dc1aed4
r4ppz <johnreyrabosa.f@gmail.com>
2025-04-18 14:18:23 +0800

    improvements

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,3 +8,28 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--
+
+-- Resize windows using Ctrl + Arrow keys
+map("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+map("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+map("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+map("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+
+-- Keybinding for toggling CopilotChat (open/close)
+map("n", "<Leader>cc", ":CopilotChatToggle<CR>", { noremap = true, silent = true })
+
+-- Keybinding for submitting a prompt in insert mode
+map("i", "<C-s>", ":CopilotChatSubmitPrompt<CR>", { noremap = true, silent = true })
+
+-- Keybinding for accepting the nearest diff in CopilotChat
+map("n", "<Leader>ga", ":CopilotChatAcceptNearestDiff<CR>", { noremap = true, silent = true })
+
+-- Keybinding for jumping to the section of the nearest diff
+map("n", "<Leader>gj", ":CopilotChatJumpToDiffSection<CR>", { noremap = true, silent = true })
+
+-- Keybinding for yanking the nearest diff to register
+map("n", "<Leader>gy", ":CopilotChatYankNearestDiff<CR>", { noremap = true, silent = true })
+
+-- Keybinding for showing current chat context
+map("n", "<Leader>gc", ":CopilotChatShowContext<CR>", { noremap = true, silent = true })
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -16,13 +16,43 @@ return {
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+
+  -- CopilotChat plugin
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css"
-  		},
-  	},
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- For curl, log, and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    cmd = { "CopilotChatOpen", "CopilotChatToggle" },
+    opts = {
+      -- Configuration options
+    },
+    config = function()
+      require("CopilotChat").setup({
+        model = 'o4-mini',
+        agent = 'copilot',
+        -- Optional configuration for CopilotChat
+      })
+    end
+  },
+
+  {
+    "ibhagwan/fzf-lua",
+    lazy = false, -- Load immediately
+    config = function()
+      require('fzf-lua').setup({}) -- You can add custom configurations here if needed
+      require('fzf-lua').register_ui_select()
+    end,
   },
 }
